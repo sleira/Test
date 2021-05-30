d9743512288bbf381ae37e81a24c5a848d96c9c6
Sharon Leira Rojas <84730310+sleira@users.noreply.github.com>
2021-05-30 15:58:52 +0200

    Add files via upload

---
+++ ejemplo3_test_Unittest.py
@@ -0,0 +1,21 @@
+# Se importa el Modulo Unittest
+# Se van a usar tres metodos de cadena de Pruebas
+#test_string_equality, test_string_case, test_is_string_upper
+import unittest
+class TestingStringMethods(unittest.TestCase):
+
+   # Este método prueba si dos cadenas son iguales
+   def test_string_equality(self):
+      self.assertEqual('ssp' * 5, 'sspsspsspsspsspssp')
+
+   # Este método prueba si dos casos de cadena son iguales
+   def test_string_case(self):
+      self.assertEqual('tutorialspoint'.upper(), 'TUTORIALSPOINT')
+
+   # Este método prueba si la cadena está en mayúsculas o no
+   def test_is_string_upper(self):
+      self.assertTrue('TUTORIALSPOINT'.isupper())
+      self.assertFalse('TUTORIALSpoint'.isupper())
+
+
+unittest.main()
